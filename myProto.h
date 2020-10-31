52232e65cdbe38d6d0820b447e61f048bbc73fb5
NastiaGusev <49269198+NastiaGusev@users.noreply.github.com>
2020-10-31 19:08:33 +0200

    Add files via upload

---
+++ myProto.h
@@ -0,0 +1,12 @@
+#pragma once
+#define MAX_LINE 64
+#define HISTOGRAM_SIZE 256
+
+
+int* cuda_Task(int* arr, int size);
+int* openMP_Task(int* src_arr,int size);
+int* calculateHistogram(int *arr, unsigned int size);
+void mergeTasks(int* dest, int* src);
+char *readInput();
+void printArr(int* arr, int size);
+int *get_nums(char *buffer, int *size);
